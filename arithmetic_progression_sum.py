54778a4c2857a6d8e763e49ee70a33d069d61384
Maxim Dashkovskiy <maxdashk3@gmail.com>
2024-12-02 16:13:06 +0200

    added initial project

---
+++ arithmetic_progression_sum.py
@@ -0,0 +1,3 @@
+def calculate(num):
+    progression = [1+3*i for i in range(num)]
+    return sum(progression)
---
+++ main.py
@@ -0,0 +1,5 @@
+import arithmetic_progression_sum as aps
+print('Welcome!')
+num = int(input('Enter the number:'))
+result = aps.calculate(num)
+print(f'The sum is: {result}')
