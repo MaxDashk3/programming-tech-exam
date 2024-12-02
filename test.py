d1a06b38bc24a838ac60a79b48670a6880054bc8
Maxim Dashkovskiy <maxdashk3@gmail.com>
2024-12-02 16:19:41 +0200

    added test

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+import arithmetic_progression_sum as app
+
+class AppTests(unittest.TestCase):
+    def test_calculate(self):
+        self.assertEqual(12, app.calculate(3))
+        self.assertEqual(22, app.calculate(4))
+
+    def Teardown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
